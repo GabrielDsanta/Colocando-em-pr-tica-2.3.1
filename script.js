8e9f0db9bce24d364e3796eede4f298edefe91b6
GabrielDSanta <eloviveiros@hotmail.com>
2022-11-22 11:01:15 -0300

    Exercicio finalizado

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
 
 
-const ChangeDisplayButton = document.querySelector('#ChangeDisplay')
-const ChangeDisplayToGridBtn = document.querySelector('#ChangeDisplayToGrid')
-const Form = document.querySelector('form')
+const options = document.querySelector('select')
+const PhoneSection = document.querySelectorAll('.PhoneSection')
 
-ChangeDisplayButton.addEventListener('click', (e) => {
-    e.preventDefault()
-    Form.style.flexDirection = 'column'
-})
+options.addEventListener('change', () => {
+    const PhoneCards = document.querySelectorAll('.PhoneItem')
+    const CardHide = document.querySelectorAll('.Hide')
+    const PhoneCard = document.createElement("div")
+    PhoneCard.classList.add('PhoneItem')
 
-ChangeDisplayToGridBtn.addEventListener('click', (e) => {
-    e.preventDefault()
+    const PhoneImage = document.createElement("img")
+    PhoneImage.src = 'https://media.istockphoto.com/photos/frameless-smartphone-with-white-screen-isolated-on-white-background-picture-id1014068816?k=20&m=1014068816&s=170667a&w=0&h=hcSiADb75qVIfTNQMHoQIWZx4J02eASuZoHB9KeVpW4='
+    PhoneImage.width = '254'
+    PhoneImage.height = '380'
+    PhoneCard.appendChild(PhoneImage)
 
-    Form.style.display = 'grid'
-    Form.style.gridTemplateColumns = '15.625rem 15.625rem 15.625rem'
-    Form.style.gridTemplateRows = '3rem 3rem 3rem'
-    Form.style.alignContent = 'end'
-    Form.style.gap = '1.563rem;'
+    const PhoneHiddenPrice = document.createElement('h2')
+    PhoneHiddenPrice.textContent = 'R$2.800'
+    PhoneCard.appendChild(PhoneHiddenPrice)
+
+    if(options.selectedIndex == 0){
+        for(let index = 0; index <= PhoneSection.length + 3; index++) {
+            console.log(index)
+            PhoneCards[index].classList.remove('PhoneItem')
+            PhoneCards[index].classList.add('Hide')
+        }
+    }
+
+    if(options.selectedIndex == 1){
+        for (let index = 0; index < PhoneSection.length; index++) {
+            PhoneSection[index].appendChild(PhoneCard.cloneNode(true)) 
+        }
+    }
+
+    if(options.selectedIndex == PhoneSection.length){
+        for (let index = 0; index < 2; index++) {
+            PhoneSection[index].appendChild(PhoneCard.cloneNode(true)) 
+            PhoneSection[index].appendChild(PhoneCard.cloneNode(true)) 
+            PhoneSection[index].appendChild(PhoneCard.cloneNode(true)) 
+        }
+    }
+    
 })
